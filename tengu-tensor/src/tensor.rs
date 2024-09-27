ffc016a7d7522d928a5bff0f729f44e9cf22bc1f
Teia Leusten <teia.leusten@proton.me>
2024-09-27 11:40:05 +0300

    Add tensor tests

--- tengu-tensor/src/tensor.rs
+++ tengu-tensor/src/tensor.rs
@@ -4,52 +4,9 @@ use tengu_wgpu::{Buffer, BufferUsage, ByteSize};
 
 use crate::{Emit, Expression, Probable, Probe, Tengu};
 
-pub struct TensorBuilder {
-    shape: Vec<usize>,
-    count: usize,
-    tengu: Arc<Tengu>,
-}
+const LABEL_LENGTH: usize = 6;
 
-impl TensorBuilder {
-    pub fn new(tengu: &Arc<Tengu>, shape: impl Into<Vec<usize>>) -> Self {
-        let shape = shape.into();
-        let count = shape.iter().product();
-        Self {
-            shape,
-            count,
-            tengu: Arc::clone(tengu),
-        }
-    }
-
-    pub fn empty<T>(self) -> Tensor<T> {
-        let size = self.count.bytes();
-        let buffer = self.tengu.device().buffer::<T>(BufferUsage::ReadWrite).empty(size);
-        Tensor {
-            label: random_string::generate(6, ALPHA),
-            buffer,
-            count: self.count,
-            shape: self.shape,
-            tengu: self.tengu,
-            probe: None,
-        }
-    }
-
-    pub fn init<T>(self, data: &[T]) -> Tensor<T>
-    where
-        T: bytemuck::Pod,
-    {
-        assert_eq!(data.len(), self.count, "data length does not match shape");
-        let buffer = self.tengu.device().buffer::<T>(BufferUsage::ReadWrite).with_data(data);
-        Tensor {
-            label: random_string::generate(6, ALPHA),
-            buffer,
-            count: self.count,
-            shape: self.shape,
-            tengu: self.tengu,
-            probe: None,
-        }
-    }
-}
+// Tensor implementation
 
 pub struct Tensor<T> {
     label: String,
@@ -67,6 +24,10 @@ impl<T> Tensor<T> {
 
     pub fn count(&self) -> usize {
         self.count
+    }
+
+    pub fn label(&self) -> &str {
+        &self.label
     }
 
     pub fn declaration(&self, group: usize, binding: usize) -> String {
@@ -89,6 +50,76 @@ impl<T> Tensor<T> {
     }
 }
 
+// Builder implementation
+
+pub struct TensorBuilder {
+    shape: Vec<usize>,
+    count: usize,
+    label: Option<String>,
+    tengu: Arc<Tengu>,
+}
+
+impl TensorBuilder {
+    pub fn new(tengu: &Arc<Tengu>, shape: impl Into<Vec<usize>>) -> Self {
+        let shape = shape.into();
+        let count = shape.iter().product();
+        Self {
+            shape,
+            count,
+            label: None,
+            tengu: Arc::clone(tengu),
+        }
+    }
+
+    pub fn with_label(mut self, label: impl Into<String>) -> Self {
+        self.label = Some(label.into());
+        self
+    }
+
+    pub fn empty<T>(mut self) -> Tensor<T> {
+        let size = self.count.bytes();
+        let buffer = self
+            .tengu
+            .device()
+            .buffer::<T>(BufferUsage::ReadWrite)
+            .empty(size);
+        Tensor {
+            label: self.label(),
+            buffer,
+            count: self.count,
+            shape: self.shape,
+            tengu: self.tengu,
+            probe: None,
+        }
+    }
+
+    pub fn init<T>(mut self, data: &[T]) -> Tensor<T>
+    where
+        T: bytemuck::Pod,
+    {
+        assert_eq!(data.len(), self.count, "data length does not match shape");
+        let buffer = self
+            .tengu
+            .device()
+            .buffer::<T>(BufferUsage::ReadWrite)
+            .with_data(data);
+        Tensor {
+            label: self.label(),
+            buffer,
+            count: self.count,
+            shape: self.shape,
+            tengu: self.tengu,
+            probe: None,
+        }
+    }
+
+    fn label(&mut self) -> String {
+        self.label
+            .take()
+            .unwrap_or_else(|| random_string::generate(LABEL_LENGTH, ALPHA))
+    }
+}
+
 // Trait implementations
 
 impl<T: 'static> Emit for Tensor<T> {
@@ -101,7 +132,9 @@ impl<T> Probable<T> for Tensor<T> {
     fn probe(&mut self) -> &Probe<T> {
         let probe = Probe::new(&self.tengu, self.count);
         self.probe = Some(probe);
-        self.probe.as_ref().expect("Should have probe after setting one")
+        self.probe
+            .as_ref()
+            .expect("Should have probe after setting one")
     }
 }
 
@@ -111,6 +144,65 @@ impl<T> Add for Tensor<T> {
     type Output = Expression<T>;
 
     fn add(self, other: Tensor<T>) -> Self::Output {
+        assert_eq!(self.shape, other.shape, "tensor shapes should match");
         Expression::add(self, other)
+    }
+}
+
+// Tests
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn tensor_builder() {
+        let tengu = Tengu::new().await.unwrap();
+        let tensor = tengu.tensor([3, 3, 3]).empty::<f32>();
+        assert_eq!(tensor.count(), 27);
+        assert_eq!(tensor.shape(), &[3, 3, 3]);
+    }
+
+    #[tokio::test]
+    async fn tensor_declaration() {
+        let tengu = Tengu::new().await.unwrap();
+        let tensor = tengu.tensor([3, 3, 3]).with_label("tz").empty::<f32>();
+        let declaration = tensor.declaration(1, 2);
+        assert_eq!(
+            declaration,
+            "@group(1) @binding(2) var<storage, read_write> tz: array<f32>"
+        );
+    }
+
+    #[tokio::test]
+    async fn tensor_label() {
+        let tengu = Tengu::new().await.unwrap();
+        let tensor = tengu.tensor([3, 3, 3]).empty::<f32>();
+        assert_eq!(tensor.label().len(), LABEL_LENGTH);
+        assert!(tensor.label().chars().all(|c| c.is_alphabetic()));
+
+        let tensor = tengu.tensor([3]).init(&[1, 2, 3]);
+        assert_eq!(tensor.label().len(), LABEL_LENGTH);
+        assert!(tensor.label().chars().all(|c| c.is_alphabetic()));
+    }
+
+    #[tokio::test]
+    async fn tensor_emit() {
+        let tengu = Tengu::new().await.unwrap();
+        let tensor = tengu.tensor([3, 3, 3]).with_label("tenzor").empty::<f32>();
+        assert_eq!(tensor.emit(), "tenzor[idx]");
+
+        let tensor = tengu.tensor([3]).with_label("tenzor").init(&[1, 2, 3]);
+        assert_eq!(tensor.label.len(), LABEL_LENGTH);
+        assert!(tensor.label.chars().all(|c| c.is_alphabetic()));
+    }
+
+    #[tokio::test]
+    #[should_panic]
+    async fn test_shape_mismatch() {
+        let tengu = Tengu::new().await.unwrap();
+        let lhs = tengu.tensor([1, 2, 3]).empty::<f32>();
+        let rhs = tengu.tensor([3, 2, 1]).empty::<f32>();
+        let _ = lhs + rhs;
     }
 }
