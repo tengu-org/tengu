96a8dce07af33d0e4461c2c75f544e92c71fdfbe
Teia Leusten <teia.leusten@proton.me>
2024-09-29 20:14:09 +0300

    Fix probing in addition example

--- tengu-tensor/examples/add.rs
+++ tengu-tensor/examples/add.rs
@@ -7,12 +7,12 @@ pub async fn main() {
     let a = tengu.tensor([2, 2]).init(&[1.0, 2.0, 3.0, 4.0]);
     let b = tengu.tensor([2, 2]).init(&[5.0, 6.0, 7.0, 8.0]);
 
-    // Create and run computation graph.
+    // Create computation graph.
     let mut graph = tengu.graph();
     graph.add_block("addition").add_computation("out", a + b);
-    graph.compute();
 
-    // Probe the result.
+    // Run and probe the result.
     let result = graph.probe("addition", "out").unwrap();
+    graph.compute();
     println!("{:?}", result.retrieve::<f32>().await.unwrap());
 }
