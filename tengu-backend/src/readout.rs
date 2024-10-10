fe27bce38b7978553f58c4a6b70df97721e39699
Teia Leusten <teia.leusten@proton.me>
2024-10-10 21:34:51 +0300

    Add documentation for tengu-backend readout trait

--- tengu-backend/src/readout.rs
+++ tengu-backend/src/readout.rs
@@ -1,8 +1,39 @@
+//! Readout abstraction for the Tengu backend.
+//!
+//! This module defines the `Readout` trait, which represents an abstraction for readout
+//! operations in the Tengu backend. Readout operations are essential for extracting results
+//! from the tensor graph after computations have been completed.
+//!
+//! # Traits
+//!
+//! - `Readout`: A trait for handling readout operations in the Tengu backend.
+//!
+//! # Related Traits
+//!
+//! - `Backend`: A trait representing the backend where computations are performed.
+//!
 use crate::Backend;
 
+/// A trait for handling readout operations in the Tengu backend.
+///
+/// This trait defines the necessary operations and associated types required for
+/// extracting results from computations performed on the backend. Implementors of
+/// this trait must specify the type of backend used and provide a method for committing
+/// the readout process.
+///
+/// # Associated Types
+/// - `Backend`: The type of the backend used for computations.
+///
+/// # Methods
+/// - `commit`: Runs the readout process using the specified processor.
 pub trait Readout<'a> {
-    /// Type of the backend.
+    /// The type of the backend.
     type Backend: Backend;
 
+    /// Runs the readout process using the specified processor to locate tensors.
+    ///
+    /// # Parameters
+    /// - `processor`: A reference to the processor used for finding tensors and performing
+    ///   the readout process on them.
     fn commit(&mut self, processor: &<Self::Backend as Backend>::Processor<'_>);
 }
