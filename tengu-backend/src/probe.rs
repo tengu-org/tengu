81c908921e1a8c5f962b2692ccbe117e583eca01
Teia Leusten <teia.leusten@proton.me>
2024-10-11 10:13:22 +0300

    Add documentation for tengu-backend probe trait

--- tengu-backend/src/probe.rs
+++ tengu-backend/src/probe.rs
@@ -1,8 +1,26 @@
+//! This module defines the `Probe` trait, which is used for retrieving data in an asynchronous manner
+//! from different types that implement the `IOType` trait. The `Probe` trait provides an interface for
+//! extracting data from a tensor buffer.
+//!
 #![allow(async_fn_in_trait)]
 
 use crate::{IOType, Result};
 
+/// The `Probe` trait defines an interface for retrieving data asynchronously
+/// from the associated tensor. The retrieved data is stored in the provided buffer.
+///
+/// # Type Parameters
+/// - `T`: The type of data to be retrieved, which must implement the `IOType` trait.
 pub trait Probe<T: IOType> {
-    /// Retrieve the data from the probe.
+    /// Asynchronously retrieves the data from the probe and stores it in the provided buffer.
+    ///
+    /// # Parameters
+    /// - `buffer`: A mutable reference to a `Vec` where the retrieved data will be stored.
+    ///
+    /// # Returns
+    /// A `Result` type that indicates the success or failure of the operation.
+    ///
+    /// # Errors
+    /// This method may return an error if the data retrieval process encounters any issues.
     async fn retrieve_to(&self, buffer: &mut Vec<T>) -> Result<()>;
 }
