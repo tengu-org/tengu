52ba5a6b657d2857d9e634de0c4c60415a94bf15
Teia Leusten <teia.leusten@proton.me>
2024-10-12 17:35:34 +0300

    Add documentation for tengu-tensor expression module

--- tengu-tensor/src/expression.rs
+++ tengu-tensor/src/expression.rs
@@ -1,8 +1,16 @@
+//! Module for defining and manipulating expressions in the Tengu tensor computation framework.
+//!
+//! This module defines the `Expression` enum and associated traits for creating and managing
+//! various types of expressions, including scalar values, tensors, binary operations, casts,
+//! unary functions, and statements. It provides a comprehensive interface for constructing
+//! and processing these expressions.
+
 use as_any::AsAny;
 use tengu_backend::{Backend, Processor, StorageType};
 
 use crate::tensor::Tensor;
 use crate::Result;
+
 use cast::Cast;
 use ops::Binary;
 use statement::Statement;
@@ -14,26 +22,85 @@ mod ops;
 mod statement;
 mod unary_fn;
 
-// Node traits
+// NOTE: Expression-related traits.
 
+/// A trait for tensors to treat the uniformly irrespective of their underlying type.
+///
+/// The `Source` trait defines methods for matching to and copying links between sources.
 pub trait Source<B: Backend>: AsAny {
+    /// Checks if the source matches another source.
+    ///
+    /// # Parameters
+    /// - `other`: The other source to match against.
+    ///
+    /// # Returns
+    /// A result containing a boolean indicating whether the sources match.
     fn matches_to(&self, other: &dyn Source<B>) -> Result<bool>;
+
+    /// Copies a source tensor to another source.
+    ///
+    /// # Parameters
+    /// - `to`: The destination source to copy the tensor to.
+    /// - `linker`: A mutable reference to the linker.
+    ///
+    /// # Returns
+    /// A result indicating success or failure.
     fn copy(&self, to: &dyn Source<B>, linker: &mut B::Linker<'_>) -> Result<()>;
 }
 
+/// A trait for types that have a shape.
+///
+/// The `Shape` trait defines methods for retrieving the shape and the number of elements of a
+/// tensor or tensor expression.
 pub trait Shape {
+    /// Returns the shape of the object.
+    ///
+    /// # Returns
+    /// A slice representing the shape of the object.
     fn shape(&self) -> &[usize];
+
+    /// Returns the number of elements in the object.
+    ///
+    /// # Returns
+    /// The number of elements.
     fn count(&self) -> usize;
 }
 
+/// A trait for AST nodes in the Tengu framework. Any expression or a tensor is a node.
+///
+/// The `Node` trait extends the `Shape` trait and defines methods for visiting, finding, and cloning nodes.
 pub trait Node<B: Backend>: Shape {
+    /// Visits the node with a processor.
+    ///
+    /// # Parameters
+    /// - `processor`: A mutable reference to the processor.
+    ///
+    /// # Returns
+    /// The inner representation of the processor, as defined by `tengu-backend`.
     fn visit<'a>(&'a self, processor: &mut B::Processor<'a>) -> <B::Processor<'a> as Processor>::Repr;
+
+    /// Finds a source by its label.
+    ///
+    /// # Parameters
+    /// - `label`: The label of the source to find.
+    ///
+    /// # Returns
+    /// An optional reference to the source.
     fn find<'a>(&'a self, label: &str) -> Option<&'a dyn Source<B>>;
+
+    /// Clones the node into a boxed trait object.
+    ///
+    /// # Returns
+    /// A boxed trait object containing the cloned node.
     fn clone_box(&self) -> Box<dyn Node<B>>;
 }
 
-// Expression
+// NOTE: Expression implementation.
 
+/// An enum representing various types of expressions in the Tengu framework.
+///
+/// The `Expression` enum can represent scalar values, tensors, binary operations, casts,
+/// unary functions, and statements.
 pub enum Expression<T: StorageType, B: Backend + 'static>
 where
     T: StorageType,
@@ -52,6 +119,10 @@ where
     T: StorageType,
     B: Backend + 'static,
 {
+    /// Returns the label of the expression if it is a tensor.
+    ///
+    /// # Returns
+    /// An optional reference to the label string if the epxression is a tensor, None otherwise.
     pub fn label(&self) -> Option<&str> {
         match self {
             Self::Tensor(tensor) => Some(tensor.label()),
@@ -59,30 +130,57 @@ where
         }
     }
 
+    /// Create the cast expression.
+    ///
+    /// # Parameters
+    /// - `S`: The target storage type.
+    ///
+    /// # Returns
+    /// A new cast expression with the target storage type.
     pub fn cast<S: StorageType>(self) -> Expression<S, B> {
         Expression::Cast(Cast::new(self))
     }
 
+    /// Create the log expression.
+    ///
+    /// # Returns
+    /// A new expression representing the logarithm of the original expression.
     pub fn log(self) -> Expression<T, B> {
         Self::UnaryFn(UnaryFn::log(self))
     }
 
+    /// Create the exponential expression.
+    ///
+    /// # Returns
+    /// A new expression representing the exponential of the original expression.
     pub fn exp(self) -> Expression<T, B> {
         Self::UnaryFn(UnaryFn::exp(self))
     }
 
+    /// Creates a statement expression.
+    ///
+    /// # Parameters
+    /// - `expr`: The input expression.
+    /// - `output`: The output expression.
+    ///
+    /// # Returns
+    /// A new statement expression.
     pub fn statement(expr: Expression<T, B>, output: Expression<T, B>) -> Self {
         Self::Statement(Statement::new(expr, output))
     }
 }
 
-// Shape implementation
+// NOTE: Shape implementation.
 
 impl<T, B> Shape for Expression<T, B>
 where
     T: StorageType,
     B: Backend + 'static,
 {
+    /// Returns the shape of the expression.
+    ///
+    /// # Returns
+    /// A slice representing the shape of the expression. Scalars have shae [1].
     fn shape(&self) -> &[usize] {
         match self {
             Self::Scalar(_) => &[1],
@@ -93,7 +191,10 @@ where
             Self::Statement(statement) => statement.shape(),
         }
     }
-
+    /// Returns the number of elements in the expression.
+    ///
+    /// # Returns
+    /// The number of elements.
     fn count(&self) -> usize {
         match self {
             Self::Scalar(_) => 1,
@@ -106,17 +207,27 @@ where
     }
 }
 
-// Node implementation
+// NOTE: Node implementation.
 
 impl<T, B> Node<B> for Expression<T, B>
 where
     T: StorageType,
     B: Backend + 'static,
 {
+    /// Clones the expression into a boxed trait object.
+    ///
+    /// # Returns
+    /// A boxed trait object containing the cloned expression.
     fn clone_box(&self) -> Box<dyn Node<B>> {
         Box::new(self.clone())
     }
-
+    /// Finds a source within the expression by its label.
+    ///
+    /// # Parameters
+    /// - `label`: The label of the source to find.
+    ///
+    /// # Returns
+    /// An optional reference to the source.
     fn find<'a>(&'a self, label: &str) -> Option<&'a dyn Source<B>> {
         match self {
             Self::Scalar(_) => None,
@@ -127,7 +238,13 @@ where
             Self::Statement(statement) => statement.find(label),
         }
     }
-
+    /// Visits the expression with a processor.
+    ///
+    /// # Parameters
+    /// - `processor`: A mutable reference to the processor.
+    ///
+    /// # Returns
+    /// The inner representation used by the processor.
     fn visit<'a>(&'a self, processor: &mut B::Processor<'a>) -> <B::Processor<'a> as Processor>::Repr {
         match self {
             Self::Scalar(scalar) => processor.scalar(*scalar),
@@ -140,9 +257,13 @@ where
     }
 }
 
-// Clone
+// NOTE: Clone implementation.
 
 impl<T: StorageType, B: Backend> Clone for Expression<T, B> {
+    /// Clones the expression.
+    ///
+    /// # Returns
+    /// A new expression that is a clone of the original.
     fn clone(&self) -> Self {
         match self {
             Self::Scalar(scalar) => Self::Scalar(*scalar),
