edaa4878eff0baef041f03885d20bcb585760c36
Teia Leusten <teia.leusten@proton.me>
2024-10-21 16:30:18 +0300

    Add README for tengu-backend-tensor

--- tengu-backend-tensor/src/lib.rs
+++ tengu-backend-tensor/src/lib.rs
@@ -2,6 +2,14 @@
 //! adhere to in order to support tensor operations. The crate is intended to be consumed by user-facing
 //! crates such as `tengu-graph`.
 //!
+//! ## Features
+//!
+//! - GPU Compatibility: Defines traits (IOType and StorageType) that ensure data types used in tensors are compatible
+//!   with GPU operations and can be safely transferred between CPU and GPU memory.
+//! - Tensor Abstraction: Provides the Tensor trait, representing a tensor with essential methods for managing and retrieving
+//!   tensor data asynchronously.
+//! - Type Flexibility: Supports various types (f32, u32, i32) for tensor elements, enabling efficient computation and data management.
+//!
 //! ## Modules
 //!
 //! - `tensor`: Defines the `Tensor` trait, which represents a tensor in the computation framework.
@@ -9,6 +17,14 @@
 //! - `types`: Defines traits used throughout the crate, namely `IOType` and `StorageType`,
 //!   which represent different categories of data types used in tensor computations.
 //!
+//! ## Traits
+//! - IOType: Represents types transferable between CPU and GPU, ensuring safe memory management.
+//! - StorageType: Specifies types that can be stored on the GPU, with an associated IOType for CPU transfers.
+//! - Tensor: Defines the core interface for tensors, including methods like:
+//!    - label: Returns the tensor's label.
+//!    - count: Provides the number of elements.
+//!    - shape: Retrieves the tensor's shape.
+//!    - retrieve: Asynchronously accesses tensor data.
 
 mod tensor;
 mod types;
