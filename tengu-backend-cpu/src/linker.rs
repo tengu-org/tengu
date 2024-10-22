c3503202caa8ee0be056f3ef2ef2ed1f9163cf10
Teia Leusten <teia.leusten@proton.me>
2024-10-22 15:52:44 +0300

    Implement linking

--- tengu-backend-cpu/src/linker.rs
+++ tengu-backend-cpu/src/linker.rs
@@ -7,9 +7,9 @@ impl tengu_backend::Linker for Linker {
 
     fn copy_link<T: tengu_backend_tensor::StorageType>(
         &mut self,
-        _from: &<Self::Backend as tengu_backend::Backend>::Tensor<T>,
-        _to: &<Self::Backend as tengu_backend::Backend>::Tensor<T>,
+        from: &<Self::Backend as tengu_backend::Backend>::Tensor<T>,
+        to: &<Self::Backend as tengu_backend::Backend>::Tensor<T>,
     ) {
-        todo!()
+        from.copy_to(to);
     }
 }
