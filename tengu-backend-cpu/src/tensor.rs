6cdb5b18b6746bdd56b0cdedfa06e458edaf4dc2
Teia Leusten <teia.leusten@proton.me>
2024-10-24 15:21:10 +0300

    Add some tests to tengu-backend-cpu

--- tengu-backend-cpu/src/tensor.rs
+++ tengu-backend-cpu/src/tensor.rs
@@ -95,3 +95,90 @@ impl<T: StorageType> Clone for Tensor<T> {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use pretty_assertions::assert_eq;
+    use std::collections::HashSet;
+    use std::rc::Rc;
+
+    use tengu_backend::{Backend, Processor};
+    use tengu_backend_tensor::{Function, Operator, Type};
+
+    use crate::Backend as CPUBackend;
+
+    #[test]
+    fn scalar() {
+        let probes = HashSet::new();
+        let mut processor = CPUBackend.processor(&probes);
+        let scalar = processor.scalar(2.37);
+        let scalar = scalar.as_ref::<f32>();
+        assert_eq!(scalar.shape, [1]);
+        assert_eq!(scalar.data.borrow().len(), 1);
+        assert_eq!(scalar.data.borrow().to_vec(), [2.37]);
+    }
+
+    #[test]
+    fn cast() {
+        let probes = HashSet::new();
+        let backend = Rc::new(CPUBackend);
+        let mut processor = backend.processor(&probes);
+        let a = backend.tensor("a", [2, 2], &[1, 2, 3, 4]);
+        let a = processor.var(&a);
+        let cast_a = processor.cast(a, Type::F32);
+        let cast_a = cast_a.as_ref::<f32>();
+        assert_eq!(cast_a.shape, [2, 2]);
+        assert_eq!(cast_a.data.borrow().len(), 4);
+        assert_eq!(cast_a.data.borrow().to_vec(), [1.0, 2.0, 3.0, 4.0]);
+    }
+
+    #[test]
+    fn unary_fn() {
+        let probes = HashSet::new();
+        let backend = Rc::new(CPUBackend);
+        let mut processor = backend.processor(&probes);
+        let a = backend.tensor("a", [1], &[1.0]);
+        let a = processor.var(&a);
+        let exp = processor.unary_fn(a, Function::Exp);
+        let cast_a = exp.as_ref::<f32>();
+        assert_eq!(cast_a.shape, [1]);
+        assert_eq!(cast_a.data.borrow().len(), 1);
+        assert_eq!(cast_a.data.borrow().to_vec(), [std::f32::consts::E]);
+    }
+
+    #[test]
+    fn binary() {
+        let probes = HashSet::new();
+        let backend = Rc::new(CPUBackend);
+        let mut processor = backend.processor(&probes);
+        let a = backend.tensor("a", [4], &[1, 2, 3, 4]);
+        let b = backend.tensor("b", [4], &[5, 6, 7, 8]);
+        let a = processor.var(&a);
+        let b = processor.var(&b);
+        let a_add_b = processor.binary(a, b, Operator::Mul);
+        println!("a_add_b is {}", a_add_b.variant());
+        let a_add_b = a_add_b.as_ref::<i32>();
+        assert_eq!(a_add_b.shape, [4]);
+        assert_eq!(a_add_b.data.borrow().len(), 4);
+        assert_eq!(a_add_b.data.borrow().to_vec(), [5, 12, 21, 32]);
+    }
+
+    #[test]
+    fn statement() {
+        let probes = HashSet::new();
+        let backend = Rc::new(CPUBackend);
+        let mut processor = backend.processor(&probes);
+        let a = backend.tensor("a", [4], &[1, 2, 3, 4]);
+        let b = backend.tensor("b", [4], &[5, 6, 7, 8]);
+        let c = backend.zero::<i32>("c", [4]);
+        let a = processor.var(&a);
+        let b = processor.var(&b);
+        let a_add_b = processor.binary(a, b, Operator::Add);
+        let c = processor.var(&c);
+        let statement = processor.statement(c, a_add_b);
+        let statement = statement.as_ref::<i32>();
+        assert_eq!(statement.shape, [4]);
+        assert_eq!(statement.data.borrow().len(), 4);
+        assert_eq!(statement.data.borrow().to_vec(), [6, 8, 10, 12]);
+    }
+}
